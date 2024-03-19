c11929f0f5760107d6b6bbfb459e5b3ee610cf66
Usuario <Usuario@CPD02-M04>
2024-03-19 12:27:05 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleSALARIO/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleSALARIO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual a forma de contratação? \nA- assalariado\nC- comissionado\nH-horista");
+                string contrat = Console.ReadLine().ToUpper();
+            double salario
+            switch (contrat)
+            {
+                case "A":
+                    Console.WriteLine("digite o salário bruto");
+                   double salario = double.Parse(Console.ReadLine());
+                    Console.WriteLine("informe o valor do desconto");
+                    double desc = double.Parse(Console.ReadLine());
+                    result = salario - desco;
+                    break;
+
+            }
+
+        }
+    }
+}
